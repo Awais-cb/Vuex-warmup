69f868b1001beff2f5be226c28893b5eed74cb85
Awais-cb <awaisfiazit@gmail.com>
2018-09-18 11:08:32 +0500

    good changes made

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// loading api related functions
-import appConfigs from '../includes/api/appConfigs'
-
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
